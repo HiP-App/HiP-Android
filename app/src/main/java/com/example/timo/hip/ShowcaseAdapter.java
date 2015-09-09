7ddfa044ac67375aaa6cc93eab1915883963f39b
christian <stromber@mail.uni-paderborn.de>
2015-09-09 10:44:47 +0200

    add thumbnail for AR showcase

--- app/src/main/java/com/example/timo/hip/ShowcaseAdapter.java
+++ app/src/main/java/com/example/timo/hip/ShowcaseAdapter.java
@@ -64,7 +64,7 @@ public class ShowcaseAdapter extends BaseAdapter {
             {
                 R.drawable.showcase_main,
                 R.drawable.showcase_main,
-                R.drawable.showcase_main,
+                R.drawable.showcase_ar,
                 R.drawable.showcase_imgview,
                 R.drawable.showcase_imgboundaries,
                 R.drawable.showcase_main,
