d8f080cbdc7768171f6d7e68c5a5ea80d79e64d3
christian <stromber@mail.uni-paderborn.de>
2015-09-11 14:49:05 +0200

    added details view and json format to POI example

--- app/src/main/assets/samples/Point$Of$Interest_Multiple$Pois/js/multiplepois.js
+++ app/src/main/assets/samples/Point$Of$Interest_Multiple$Pois/js/multiplepois.js
@@ -1,11 +1,15 @@
 // implementation of AR-Experience (aka "World")
 var World = {
+	// you may request new data from server periodically, however: in this sample data is only requested once
+	isRequestingData: false,
+
 	// true once data was fetched
 	initiallyLoadedData: false,
 
 	// different POI-Marker assets
 	markerDrawable_idle: null,
 	markerDrawable_selected: null,
+	markerDrawable_directionIndicator: null,
 
 	// list of AR.GeoObjects that are currently shown in the scene / World
 	markerList: [],
@@ -13,14 +17,19 @@ var World = {
 	// The last selected marker
 	currentMarker: null,
 
+	locationUpdateCounter: 0,
+	updatePlacemarkDistancesEveryXLocationUpdates: 10,
+
 	// called to inject new POI data
 	loadPoisFromJsonData: function loadPoisFromJsonDataFn(poiData) {
+
 		// empty list of visible markers
 		World.markerList = [];
 
 		// start loading marker assets
 		World.markerDrawable_idle = new AR.ImageResource("assets/marker_idle.png");
 		World.markerDrawable_selected = new AR.ImageResource("assets/marker_selected.png");
+		World.markerDrawable_directionIndicator = new AR.ImageResource("assets/indi.png");
 
 		// loop through POI-information and create an AR.GeoObject (=Marker) per POI
 		for (var currentPlaceNr = 0; currentPlaceNr < poiData.length; currentPlaceNr++) {
@@ -30,17 +39,24 @@ var World = {
 				"longitude": parseFloat(poiData[currentPlaceNr].longitude),
 				"altitude": parseFloat(poiData[currentPlaceNr].altitude),
 				"title": poiData[currentPlaceNr].name,
-				"description": poiData[currentPlaceNr].description
+				"description": poiData[currentPlaceNr].description,
+				"image": poiData[currentPlaceNr].image
 			};
 
-			/*
-				To be able to deselect a marker while the user taps on the empty screen, 
-				the World object holds an array that contains each marker.
-			*/
 			World.markerList.push(new Marker(singlePoi));
 		}
 
+		// updates distance information of all placemarks
+		World.updateDistanceToUserValues();
+
 		World.updateStatusMessage(currentPlaceNr + ' places loaded');
+	},
+
+	// sets/updates distances of all makers so they are available way faster than calling (time-consuming) distanceToUser() method all the time
+	updateDistanceToUserValues: function updateDistanceToUserValuesFn() {
+		for (var i = 0; i < World.markerList.length; i++) {
+			World.markerList[i].distanceToUser = World.markerList[i].markerObject.locations[0].distanceToUser();
+		}
 	},
 
 	// updates status message shon in small "i"-button aligned bottom center
@@ -61,67 +77,133 @@ var World = {
 	// location updates, fired every time you call architectView.setLocation() in native environment
 	locationChanged: function locationChangedFn(lat, lon, alt, acc) {
 
-		/*
-			The custom function World.onLocationChanged checks with the flag World.initiallyLoadedData if the function was already called. With the first call of World.onLocationChanged an object that contains geo information will be created which will be later used to create a marker using the World.loadPoisFromJsonData function.
-		*/
+		// request data if not already present
 		if (!World.initiallyLoadedData) {
-			/* 
-				requestDataFromLocal with the geo information as parameters (latitude, longitude) creates different poi data to a random location in the user's vicinity.
-			*/
 			World.requestDataFromLocal(lat, lon);
 			World.initiallyLoadedData = true;
+		} else if (World.locationUpdateCounter === 0) {
+			// update placemark distance information frequently, you max also update distances only every 10m with some more effort
+			World.updateDistanceToUserValues();
 		}
+
+		// helper used to update placemark information every now and then (e.g. every 10 location upadtes fired)
+		World.locationUpdateCounter = (++World.locationUpdateCounter % World.updatePlacemarkDistancesEveryXLocationUpdates);
 	},
+
+	/*
+		POIs usually have a name and sometimes a quite long description. 
+		Depending on your content type you may e.g. display a marker with its name and cropped description but allow the user to get more information after selecting it.
+	*/
 
 	// fired when user pressed maker in cam
 	onMarkerSelected: function onMarkerSelectedFn(marker) {
-
-		// deselect previous marker
-		if (World.currentMarker) {
-			if (World.currentMarker.poiData.id == marker.poiData.id) {
-				return;
-			}
-			World.currentMarker.setDeselected(World.currentMarker);
-		}
-
-		// highlight current one
-		marker.setSelected(marker);
 		World.currentMarker = marker;
+
+		/*
+			In this sample a POI detail panel appears when pressing a cam-marker (the blue box with title & description), 
+			compare index.html in the sample's directory.
+		*/
+		// update panel values
+		$("#poi-detail-title").html(marker.poiData.title);
+		$("#poi-detail-description").html(marker.poiData.description);
+		$("#poi-detail-image").attr("src",marker.poiData.image);
+//		if(marker.poiData.id < 10){
+//			document.getElementById('poi-detail-image').src = "assets/Liboribrunnen.jpg";
+//		} else {
+//			document.getElementById('poi-detail-image').src = "assets/Dom.jpg";
+//		}
+
+		// distance and altitude are measured in meters by the SDK. You may convert them to miles / feet if required.
+		var distanceToUserValue = (marker.distanceToUser > 999) ? ((marker.distanceToUser / 1000).toFixed(2) + " km") : (Math.round(marker.distanceToUser) + " m");
+
+		$("#poi-detail-distance").html(distanceToUserValue);
+
+		// show panel
+		$("#panel-poidetail").panel("open", 123);
+
+		$(".ui-panel-dismiss").unbind("mousedown");
+
+		// deselect AR-marker when user exits detail screen div.
+		$("#panel-poidetail").on("panelbeforeclose", function(event, ui) {
+			World.currentMarker.setDeselected(World.currentMarker);
+		});
 	},
 
 	// screen was clicked but no geo-object was hit
 	onScreenClick: function onScreenClickFn() {
-		if (World.currentMarker) {
-			World.currentMarker.setDeselected(World.currentMarker);
-		}
+		// you may handle clicks on empty AR space too
 	},
 
-	// request POI data
-	requestDataFromLocal: function requestDataFromLocalFn(centerPointLatitude, centerPointLongitude) {
-		var poisToCreate = 20;
-		var poiData = [];
+	// returns distance in meters of placemark with maxdistance * 1.1
+	getMaxDistance: function getMaxDistanceFn() {
 
-		for (var i = 0; i < poisToCreate; i++) {
-			poiData.push({
-				"id": (i + 1),
-				"longitude": (centerPointLongitude + (Math.random() / 5 - 0.1)),
-				"latitude": (centerPointLatitude + (Math.random() / 5 - 0.1)),
-				"description": ("This is the description of POI#" + (i + 1)),
-				"altitude": "100.0",
-				"name": ("POI#" + (i + 1))
-			});
-		}
-		World.loadPoisFromJsonData(poiData);
+		// sort palces by distance so the first entry is the one with the maximum distance
+		World.markerList.sort(World.sortByDistanceSortingDescending);
+
+		// use distanceToUser to get max-distance
+		var maxDistanceMeters = World.markerList[0].distanceToUser;
+
+		// return maximum distance times some factor >1.0 so ther is some room left and small movements of user don't cause places far away to disappear.
+		return maxDistanceMeters * 1.1;
+	},
+
+	/*
+		In case the data of your ARchitect World is static the content should be stored within the application.
+		Create a JavaScript file (e.g. myJsonData.js) where a globally accessible variable is defined.
+		Include the JavaScript in the ARchitect Worlds HTML by adding <script src="js/myJsonData.js"/> to make POI information available anywhere in your JavaScript.
+	*/
+
+	// request POI data
+	requestDataFromLocal: function requestDataFromLocalFn(lat, lon) {
+
+		var poisNearby = Helper.bringPlacesToUser(myJsonData, lat, lon);
+		World.loadPoisFromJsonData(poisNearby);
+
+		/*
+		For demo purpose they are relocated randomly around the user using a 'Helper'-function.
+		Comment out previous 2 lines and use the following line > instead < to use static values 1:1.
+		*/
+
+		// World.loadPoisFromJsonData(myJsonData);
+	},
+
+
+	// helper to sort places by distance
+	sortByDistanceSorting: function(a, b) {
+		return a.distanceToUser - b.distanceToUser;
+	},
+
+	// helper to sort places by distance, descending
+	sortByDistanceSortingDescending: function(a, b) {
+		return b.distanceToUser - a.distanceToUser;
 	}
 
 };
 
-/* 
-	Set a custom function where location changes are forwarded to. There is also a possibility to set AR.context.onLocationChanged to null. In this case the function will not be called anymore and no further location updates will be received. 
-*/
+var Helper = {
+
+	/*
+		For demo purpose only, this method takes poi data and a center point (latitude, longitude) to relocate the given places randomly around the user
+	*/
+	bringPlacesToUser: function bringPlacesToUserFn(poiData, latitude, longitude) {
+		for (var i = 0; i < poiData.length; i++) {
+			poiData[i].latitude = latitude + (Math.random() / 5 - 0.1);
+			poiData[i].longitude = longitude + (Math.random() / 5 - 0.1);
+			/*
+			Note: setting altitude to '0'
+			will cause places being shown below / above user,
+			depending on the user 's GPS signal altitude.
+				Using this contant will ignore any altitude information and always show the places on user-level altitude
+			*/
+			poiData[i].altitude = AR.CONST.UNKNOWN_ALTITUDE;
+		}
+		return poiData;
+	}
+}
+
+
+/* forward locationChanges to custom function */
 AR.context.onLocationChanged = World.locationChanged;
 
-/*
-	To detect clicks where no drawable was hit set a custom function on AR.context.onScreenClick where the currently selected marker is deselected.
-*/
+/* forward clicks in empty area to World */
 AR.context.onScreenClick = World.onScreenClick;
