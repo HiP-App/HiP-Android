b4efa39379b14246062515989560979d70995d3e
christian <stromber@mail.upb.de>
2016-01-06 11:15:38 +0100

    included switch back to MainActivity to test added special character "ä" to testcase

--- app/src/androidTest/java/de/upb/hip/mobile/activities/MainActivityTest.java
+++ app/src/androidTest/java/de/upb/hip/mobile/activities/MainActivityTest.java
@@ -5,6 +5,7 @@ import android.support.test.InstrumentationRegistry;
 import static android.support.test.espresso.Espresso.*;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -17,6 +18,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     private MainActivity activity;
 
     private static final String BUSDORFKIRCHE = "Busdorfkirche";
+    private static final String UNIPADERNORN = "Universität Paderborn";
 
     public MainActivityTest(){
         super(MainActivity.class);
@@ -33,16 +35,24 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         assertNotNull("activity is null", activity);
     }
 
-    public void testSwitchToDetailsActivity(){
+    public void testSwitchToDetailsActivityAndBack(){
         onView(withId(R.id.my_recycler_view)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(BUSDORFKIRCHE)), click()));
         onView(withId(R.id.toolbar)).check(matches(hasDescendant(withText(BUSDORFKIRCHE))));
         onView(withId(R.id.txtName)).check(matches(withText(BUSDORFKIRCHE)));
+
+        onView(withContentDescription("Navigate up")).perform(click());
+
+        onView(withId(R.id.my_recycler_view)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(UNIPADERNORN)), click()));
+        onView(withId(R.id.toolbar)).check(matches(hasDescendant(withText(UNIPADERNORN))));
+        onView(withId(R.id.txtName)).check(matches(withText(UNIPADERNORN)));
     }
 
     public void testGoogleMapsMarker() throws UiObjectNotFoundException, InterruptedException {
         UiDevice device = UiDevice.getInstance(getInstrumentation());
-        UiObject marker = device.findObject(new UiSelector().descriptionContains(BUSDORFKIRCHE));
-        marker.click();
+        UiObject marker1 = device.findObject(new UiSelector().descriptionContains(BUSDORFKIRCHE));
+//        marker1.click();
+        UiObject marker2 = device.findObject(new UiSelector().descriptionContains(UNIPADERNORN));
+        marker2.click();
     }
 
     public void testActionBarTitle(){
