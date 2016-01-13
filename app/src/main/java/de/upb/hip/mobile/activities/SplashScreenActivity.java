51207294641d7b4c1192136b91f061e6010ce803
tboegeholz <boegeholz.timo@gmail.com>
2016-01-13 10:52:11 +0100

    Added Database Check Added strings to strings.xml

--- app/src/main/java/de/upb/hip/mobile/activities/SplashScreenActivity.java
+++ app/src/main/java/de/upb/hip/mobile/activities/SplashScreenActivity.java
@@ -6,7 +6,20 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import de.upb.hip.mobile.adapters.DBAdapter;
 
 /**
  * Created by Timo on 10.01.2016.
@@ -15,6 +28,9 @@ public class SplashScreenActivity extends Activity {
 
     private TextView txtAction;
     private TextView txtWaiting;
+    private ProgressBar progBar;
+    private Button btnRetry;
+    DBAdapter dba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,32 +39,63 @@ public class SplashScreenActivity extends Activity {
 
         txtAction = (TextView) findViewById(R.id.textView_action);
         txtWaiting = (TextView) findViewById(R.id.textView_waiting);
+        progBar = (ProgressBar) findViewById(R.id.progressBar1);
+        btnRetry = (Button) findViewById(R.id.button_retry);
 
         //Check If Internet Connection
+
+        dba = new DBAdapter(this);
+        if(dba.getDocumentCount() > 0)
+        {
+            startMainActivity();
+        } else {
+            onlineCheck();
+        }
+
+    }
+
+    private void onlineCheck() {
         if(isOnline())
         {
-            // Yes: Sync Database
+            // dba.COUCHBASE_SERVER_URL;
 
-                // Show Waiting Bar
 
-                // When Finished: Start Main Activity
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            finish();
+            // DOMAIN CHECK
 
+                //DOMAIN NICHT DA
+
+                // Database not reachable
+            startMainActivity();
         } else
         {
-            // No: Show Error Messeage
-            txtAction.setText("Keine Internetverbindung.");
-            txtWaiting.setText("Bitte stellen Sie sicher, dass Sie über eine aktive Internetverbindung verfügen.");
 
+            txtAction.setText(getString(R.string.splash_screen_no_connection));
+            txtWaiting.setText(getString(R.string.splash_screen_no_connection_error_message));
+            txtWaiting.setGravity(Gravity.CENTER);
+            progBar.setVisibility(View.GONE);
+            btnRetry.setVisibility(View.VISIBLE);
         }
-        // In Case of Error: Return Error
     }
 
     private boolean isOnline() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         return netInfo != null && netInfo.isConnected();
+    }
+
+    private void startMainActivity()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onclick_retry(View v)
+    {
+        txtAction.setText(getString(R.string.splash_screen_check_connection));
+        txtWaiting.setText(getString(R.string.splash_screen_waiting));
+        progBar.setVisibility(View.VISIBLE);
+        btnRetry.setVisibility(View.GONE);
+        onlineCheck();
     }
 }
