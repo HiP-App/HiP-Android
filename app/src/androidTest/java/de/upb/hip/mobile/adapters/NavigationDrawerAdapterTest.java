d6e574622947b18ce67ef1dda50166232b880e45
Tim Lobner <lobner@mail.uni-paderborn.de>
2016-01-28 10:24:28 +0100

    Started implementing first Navigation Drawer Adapter Test

---
+++ app/src/androidTest/java/de/upb/hip/mobile/adapters/NavigationDrawerAdapterTest.java
@@ -0,0 +1,24 @@
+package de.upb.hip.mobile.adapters;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by Lobner on 20.01.2016.
+ */
+public class NavigationDrawerAdapterTest extends AndroidTestCase
+{
+    NavigationDrawerAdapter testobject;
+    public NavigationDrawerAdapterTest()
+    {
+        super();
+    }
+
+
+    /* Setup test environment */
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+//        testobject = new NavigationDrawerAdapter(getContext());
+    }
+}
