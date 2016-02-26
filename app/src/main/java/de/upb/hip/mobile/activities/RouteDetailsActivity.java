3d1a651241ba0e7462c549a024f4cf17e4952824
christian <stromber@mail.upb.de>
2016-02-26 12:26:30 +0100

    code review of RouteDetailsActivity

--- app/src/main/java/de/upb/hip/mobile/activities/RouteDetailsActivity.java
+++ app/src/main/java/de/upb/hip/mobile/activities/RouteDetailsActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2016 History in Paderborn App - Universität Paderborn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.upb.hip.mobile.activities;
 
 import android.content.Intent;
@@ -35,11 +51,20 @@ import de.upb.hip.mobile.models.Route;
 import de.upb.hip.mobile.models.RouteTag;
 import de.upb.hip.mobile.models.Waypoint;
 
+/**
+ * Activity Class for the route details view, where the details of a route are show, including a
+ * map and the possibility to start the navigation.
+ */
 public class RouteDetailsActivity extends ActionBarActivity {
 
     //We need to store the markers we add to the map so that we can identify them in the listener
-    private Map<String, Integer> markerMap = new HashMap<>();
+    private Map<String, Integer> mMarkerMap = new HashMap<>();
 
+    /**
+     * Called when the activity is created, shows the details of the route
+     *
+     * @param savedInstanceState savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,21 +74,32 @@ public class RouteDetailsActivity extends ActionBarActivity {
         showRouteDetails(route);
     }
 
+    /**
+     * Sets the texts for the description, title, duration, distance and the images for the route
+     * image and tag images.
+     * Sets the Map including the waypoints.
+     *
+     * @param route {@link de.upb.hip.mobile.models.Route}
+     */
     private void showRouteDetails(Route route) {
-        TextView description = (TextView) findViewById(R.id.activityRouteDetailsRouteDescription);
-        TextView title = (TextView) findViewById(R.id.activityRouteDetailsRouteTitle);
-        TextView duration = (TextView) findViewById(R.id.activityRouteDetailsRouteDuration);
-        TextView distance = (TextView) findViewById(R.id.activityRouteDetailsRouteDistance);
-        LinearLayout tagsLayout = (LinearLayout) findViewById(R.id.activityRouteDetailsRouteTagsLayout);
+        TextView descriptionView = (TextView) findViewById(
+                R.id.activityRouteDetailsRouteDescription);
+        TextView titleView = (TextView) findViewById(R.id.activityRouteDetailsRouteTitle);
+        TextView durationView = (TextView) findViewById(R.id.activityRouteDetailsRouteDuration);
+        TextView distanceView = (TextView) findViewById(R.id.activityRouteDetailsRouteDistance);
+        LinearLayout tagsLayout = (LinearLayout) findViewById(
+                R.id.activityRouteDetailsRouteTagsLayout);
         ImageView imageView = (ImageView) findViewById(R.id.activityRouteDetailsRouteImageView);
-        final MapFragment map = (MapFragment) getFragmentManager().findFragmentById(R.id.activityRouteDetailsMap);
+        final MapFragment map = (MapFragment) getFragmentManager().findFragmentById(
+                R.id.activityRouteDetailsMap);
 
-
-        description.setText(route.description);
-        title.setText(route.title);
+        descriptionView.setText(route.description);
+        titleView.setText(route.title);
         int durationInMinutes = route.duration / 60;
-        duration.setText(getResources().getQuantityString(R.plurals.route_activity_duration_minutes, durationInMinutes, durationInMinutes));
-        distance.setText(String.format(getResources().getString(R.string.route_activity_distance_kilometer), route.distance));
+        durationView.setText(getResources().getQuantityString(
+                R.plurals.route_activity_duration_minutes, durationInMinutes, durationInMinutes));
+        distanceView.setText(String.format(getResources().getString(
+                R.string.route_activity_distance_kilometer), route.distance));
         //Add tags
         if (route.tags != null) {
             tagsLayout.removeAllViews();
@@ -74,12 +110,11 @@ public class RouteDetailsActivity extends ActionBarActivity {
             }
         }
 
-
         //Add image
-        Attachment att = DBAdapter.getAttachment(route.id, route.imageName);
+        Attachment attachment = DBAdapter.getAttachment(route.id, route.imageName);
         try {
-            Bitmap b = BitmapFactory.decodeStream(att.getContent());
-            Drawable image = new BitmapDrawable(getResources(), b);
+            Bitmap bitmap = BitmapFactory.decodeStream(attachment.getContent());
+            Drawable image = new BitmapDrawable(getResources(), bitmap);
             imageView.setImageDrawable(image);
         } catch (CouchbaseLiteException e) {
             Log.e("routes", e.toString());
@@ -102,27 +137,28 @@ public class RouteDetailsActivity extends ActionBarActivity {
                     markerOptions.snippet(exhibit.description);
                 }
                 Marker marker = map.getMap().addMarker(markerOptions);
-                markerMap.put(marker.getId(), waypoint.exhibit_id);
+                mMarkerMap.put(marker.getId(), waypoint.exhibit_id);
                 builder.include(latLng);
             }
             LatLngBounds bounds = builder.build();
             int padding = 30; // offset from edges of the map in pixels
-            final CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+            final CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, padding);
 
             //We need to wait for the map to finish loading until we can apply the camera update
             map.getMap().setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
                 @Override
                 public void onMapLoaded() {
-                    map.getMap().animateCamera(cu);
+                    map.getMap().animateCamera(cameraUpdate);
                 }
             });
 
             map.getMap().setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
                 @Override
                 public boolean onMarkerClick(Marker marker) {
-                    if (markerMap.containsKey(marker.getId()) && markerMap.get(marker.getId()) != -1) {
+                    if (mMarkerMap.containsKey(marker.getId())
+                            && mMarkerMap.get(marker.getId()) != -1) {
                         Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
-                        intent.putExtra("exhibit-id", markerMap.get(marker.getId()));
+                        intent.putExtra("exhibit-id", mMarkerMap.get(marker.getId()));
                         startActivity(intent);
                         return true;
                     }
@@ -137,10 +173,15 @@ public class RouteDetailsActivity extends ActionBarActivity {
         map.getMap().setMyLocationEnabled(true);
     }
 
+    /**
+     * Implement the onSupportNavigateUp() for the ActionBar back button.
+     * Closes the activity and goes back to the previous activity.
+     *
+     * @return always true
+     */
     @Override
     public boolean onSupportNavigateUp() {
         finish();
         return true;
     }
-
 }
