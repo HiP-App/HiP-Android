13eeee24282f1ca29c9bef02667c63d3bcc9944c
christian <stromber@mail.upb.de>
2016-03-06 10:48:54 +0100

    code review of Route (model)

--- app/src/main/java/de/upb/hip/mobile/models/Route.java
+++ app/src/main/java/de/upb/hip/mobile/models/Route.java
@@ -1,13 +1,13 @@
 package de.upb.hip.mobile.models;
 
-import com.couchbase.lite.Document;
-import com.google.android.gms.internal.im;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Route implements Serializable{
+/**
+ * Model Class the route.
+ */
+public class Route implements Serializable {
 
     public int id;
     public String title;
@@ -18,18 +18,24 @@ public class Route implements Serializable{
     public List<RouteTag> tags;
     public String imageName;
 
-    public Route (Document document) {
-
-    // STUB
-
-    }
-
-    public Route (int id, String title, String description, ArrayList<Waypoint> waypoints, int duration, double distance, List<RouteTag> tags, String imageName)
-    {
+    /**
+     * Constructor for route.
+     *
+     * @param id          The id of the route.
+     * @param title       The title of the route.
+     * @param description The description of the route.
+     * @param wayPoints   The way points of the route.
+     * @param duration    The duration of the route in seconds.
+     * @param distance    The distance of the route in kilometer.
+     * @param tags        The tags of the route.
+     * @param imageName   The name of the image, that belongs to the route.
+     */
+    public Route(int id, String title, String description, ArrayList<Waypoint> wayPoints,
+                 int duration, double distance, List<RouteTag> tags, String imageName) {
         this.id = id;
         this.title = title;
         this.description = description;
-        this.waypoints = waypoints;
+        this.waypoints = wayPoints;
         this.duration = duration;
         this.distance = distance;
         this.tags = tags;
