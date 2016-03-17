3e4855362b52a7b9fbe51e046d661de770073bc3
christian <stromber@mail.upb.de>
2016-03-17 16:53:11 +0100

    reworked layout activity_route_navigation

--- app/src/main/java/de/upb/hip/mobile/activities/RouteNavigationActivity.java
+++ app/src/main/java/de/upb/hip/mobile/activities/RouteNavigationActivity.java
@@ -45,6 +45,7 @@ import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.osmdroid.api.IMapController;
@@ -132,7 +133,7 @@ public class RouteNavigationActivity extends Activity implements MapEventsReceiv
         mProgressDialog.setCancelable(true);
 
         // getting the map
-        GenericMapView genericMap = (GenericMapView) findViewById(R.id.map);
+        GenericMapView genericMap = (GenericMapView) findViewById(R.id.routeNavigationMap);
         MapTileProviderBasic bitmapProvider = new MapTileProviderBasic(this);
         genericMap.setTileProvider(bitmapProvider);
         mMap = genericMap.getMapView();
@@ -224,7 +225,7 @@ public class RouteNavigationActivity extends Activity implements MapEventsReceiv
         mMap.getOverlays().add(scaleBarOverlay);
 
         //Tracking system:
-        mTrackingModeButton = (Button) findViewById(R.id.buttonTrackingMode);
+        mTrackingModeButton = (Button) findViewById(R.id.routeNavigationTrackingModeButton);
         mTrackingModeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 mTrackingMode = !mTrackingMode;
@@ -480,19 +481,23 @@ public class RouteNavigationActivity extends Activity implements MapEventsReceiv
      */
     private void updateManeuverIcon(Drawable drawable) {
 
-        // set maneuver icon
-        ImageView ivManeuverIcon = (ImageView) findViewById(R.id.imageView_maneuverIcon);
-        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
-        Bitmap newImage = Bitmap.createBitmap(ivManeuverIcon.getWidth(), ivManeuverIcon.getHeight(),
-                Bitmap.Config.ARGB_8888);
+//        // set maneuver icon
+//        ImageView ivManeuverIcon = (ImageView) findViewById(R.id.routeNavigationManeuverIcon);
+//        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+//        Bitmap newImage = Bitmap.createBitmap(ivManeuverIcon.getWidth(), ivManeuverIcon.getHeight(),
+//                Bitmap.Config.ARGB_8888);
+//
+//        int maneuverIconCenterX = (ivManeuverIcon.getWidth() - bitmap.getWidth()) / 2;
+//        int maneuverIconCenterY = (ivManeuverIcon.getHeight() - bitmap.getHeight()) / 2;
+//
+//        Canvas cManeuverIcon = new Canvas(newImage);
+//        cManeuverIcon.drawBitmap(bitmap, maneuverIconCenterX, maneuverIconCenterY, null);
+//
+//        ivManeuverIcon.setImageBitmap(newImage);
 
-        int maneuverIconCenterX = (ivManeuverIcon.getWidth() - bitmap.getWidth()) / 2;
-        int maneuverIconCenterY = (ivManeuverIcon.getHeight() - bitmap.getHeight()) / 2;
 
-        Canvas cManeuverIcon = new Canvas(newImage);
-        cManeuverIcon.drawBitmap(bitmap, maneuverIconCenterX, maneuverIconCenterY, null);
-
-        ivManeuverIcon.setImageBitmap(newImage);
+        ImageView ivManeuverIcon = (ImageView) findViewById(R.id.routeNavigationManeuverIcon);
+        ivManeuverIcon.setImageBitmap(((BitmapDrawable) drawable).getBitmap());
     }
 
     /**
@@ -501,23 +506,30 @@ public class RouteNavigationActivity extends Activity implements MapEventsReceiv
     //TODO calculate position for instruction if instruction too long for one line
     private void updateInstructionInfo(String instructions) {
 
-        Paint paint = getPaintForTextInstructions();
+        // I replaced this ImageView version with TextView version, but I don't know if this is OK
+        // christian
 
-        // set maneuver instruction
-        ImageView ivManeuverInstruction = (ImageView) findViewById(R.id.imageView_routeInstruction);
-        Bitmap newImage = Bitmap.createBitmap(ivManeuverInstruction.getWidth(),
-                ivManeuverInstruction.getHeight(), Bitmap.Config.ARGB_8888);
 
-        Canvas cManeuverInfo = new Canvas(newImage);
+//        // set maneuver instruction
+//        ImageView ivManeuverInstruction = (ImageView) findViewById(R.id.routeNavigationInstruction);
+//
+//        Paint paint = getPaintForTextInstructions();
+//        Bitmap newImage = Bitmap.createBitmap(ivManeuverInstruction.getWidth(),
+//                ivManeuverInstruction.getHeight(), Bitmap.Config.ARGB_8888);
+//
+//        Canvas cManeuverInfo = new Canvas(newImage);
+//
+//        Rect bounds = new Rect();
+//        paint.getTextBounds(instructions, 0, instructions.length(), bounds);
+//
+//        int textY = cManeuverInfo.getHeight() / 2 - bounds.height() / 2;
+//        int textX = cManeuverInfo.getWidth() / 2;
+//
+//        cManeuverInfo.drawText(instructions, textX, textY, paint);
+//        ivManeuverInstruction.setImageBitmap(newImage);
 
-        Rect bounds = new Rect();
-        paint.getTextBounds(instructions, 0, instructions.length(), bounds);
-
-        int textY = cManeuverInfo.getHeight() / 2 - bounds.height() / 2;
-        int textX = cManeuverInfo.getWidth() / 2;
-
-        cManeuverInfo.drawText(instructions, textX, textY, paint);
-        ivManeuverInstruction.setImageBitmap(newImage);
+        TextView ivManeuverInstruction = (TextView) findViewById(R.id.routeNavigationInstruction);
+        ivManeuverInstruction.setText(instructions);
     }
 
     /**
@@ -525,22 +537,28 @@ public class RouteNavigationActivity extends Activity implements MapEventsReceiv
      */
     private void updateDistanceInfo(String length) {
 
-        Paint paint = getPaintForTextInstructions();
+        // I replaced this ImageView version with TextView version, but I don't know if this is OK
+        // christian
 
-        ImageView ivManeuverDistance = (ImageView) findViewById(R.id.imageView_routeDistance);
-        Bitmap newImage = Bitmap.createBitmap(ivManeuverDistance.getWidth(),
-                ivManeuverDistance.getHeight(), Bitmap.Config.ARGB_8888);
+//        Paint paint = getPaintForTextInstructions();
+//
+//        ImageView ivManeuverDistance = (ImageView) findViewById(R.id.routeNavigationDistance);
+//        Bitmap newImage = Bitmap.createBitmap(ivManeuverDistance.getWidth(),
+//                ivManeuverDistance.getHeight(), Bitmap.Config.ARGB_8888);
+//
+//        Canvas cManeuverDistance = new Canvas(newImage);
+//
+//        Rect bounds = new Rect();
+//        paint.getTextBounds(length, 0, length.length(), bounds);
+//
+//        int textY = cManeuverDistance.getHeight() / 2;
+//        int textX = cManeuverDistance.getWidth() / 2;
+//
+//        cManeuverDistance.drawText(length, textX, textY, paint);
+//        ivManeuverDistance.setImageBitmap(newImage);
 
-        Canvas cManeuverDistance = new Canvas(newImage);
-
-        Rect bounds = new Rect();
-        paint.getTextBounds(length, 0, length.length(), bounds);
-
-        int textY = cManeuverDistance.getHeight() / 2;
-        int textX = cManeuverDistance.getWidth() / 2;
-
-        cManeuverDistance.drawText(length, textX, textY, paint);
-        ivManeuverDistance.setImageBitmap(newImage);
+        TextView ivManeuverDistance = (TextView) findViewById(R.id.routeNavigationDistance);
+        ivManeuverDistance.setText(length);
     }
 
     /**
