74aaca50870ae4acc3de9a8c61cc5a616c6a809d
Tim Lobner <lobner@mail.uni-paderborn.de>
2016-03-22 16:58:21 +0100

    MainActivityTest Review

--- app/src/androidTest/java/de/upb/hip/mobile/activities/MainActivityTest.java
+++ app/src/androidTest/java/de/upb/hip/mobile/activities/MainActivityTest.java
@@ -1,30 +1,47 @@
+/*
+ * Copyright (C) 2016 History in Paderborn App - Universität Paderborn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.upb.hip.mobile.activities;
 
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.BoundedMatcher;
+import android.support.test.espresso.ViewInteraction;
 import android.support.v7.widget.Toolbar;
 import android.test.ActivityInstrumentationTestCase2;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-
-import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
 import static org.hamcrest.core.Is.is;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     private static final String BUSDORFKIRCHE = "Busdorfkirche";
-    private static final String UNIPADERNORN = "Universität Paderborn";
-    private MainActivity activity;
+    private static final String UNI_PADERBORN = "Universität Paderborn";
+    private MainActivity mActivity;
 
     public MainActivityTest(){
         super(MainActivity.class);
@@ -55,23 +72,25 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     protected void setUp() throws Exception{
         super.setUp();
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-        activity = getActivity();
+        mActivity = getActivity();
     }
 
     public void testActivityExists() {
-        assertNotNull("activity is null", activity);
+        assertNotNull("activity is null", mActivity);
     }
 
     public void testSwitchToDetailsActivityAndBack(){
-        onView(withId(R.id.mainRecyclerView)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(BUSDORFKIRCHE)), click()));
+        onView(withId(R.id.mainRecyclerView)).perform(RecyclerViewActions.actionOnItem(
+                hasDescendant(withText(BUSDORFKIRCHE)), click()));
         matchToolbarTitle(BUSDORFKIRCHE);
         onView(withId(R.id.mainRowItemName)).check(matches(withText(BUSDORFKIRCHE)));
 
         onView(withContentDescription("Navigate up")).perform(click());
 
-        onView(withId(R.id.mainRecyclerView)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(UNIPADERNORN)), click()));
-        matchToolbarTitle(UNIPADERNORN);
-        onView(withId(R.id.mainRowItemName)).check(matches(withText(UNIPADERNORN)));
+        onView(withId(R.id.mainRecyclerView)).perform(RecyclerViewActions.actionOnItem(
+                hasDescendant(withText(UNI_PADERBORN)), click()));
+        matchToolbarTitle(UNI_PADERBORN);
+        onView(withId(R.id.mainRowItemName)).check(matches(withText(UNI_PADERBORN)));
     }
 
     public void testMapAvailable() {
@@ -79,7 +98,8 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     }
 
     public void testActionBarTitle() {
-        CharSequence title = InstrumentationRegistry.getTargetContext().getString(R.string.app_name);
+        CharSequence title = InstrumentationRegistry.getTargetContext()
+                .getString(R.string.app_name);
         matchToolbarTitle(title);
     }
 }
