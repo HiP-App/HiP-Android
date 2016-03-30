728b885fc574bf1c221c391072bc288beb674ee7
Alexey Zamuraev <hichhock@mail.uni-paderborn.de>
2016-03-30 10:00:03 +0200

    CodeReview

--- app/src/androidTest/java/de/upb/hip/mobile/activities/DetailsActivityTest.java
+++ app/src/androidTest/java/de/upb/hip/mobile/activities/DetailsActivityTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2016 History in Paderborn App - Universität Paderborn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.upb.hip.mobile.activities;
 
 import android.support.test.InstrumentationRegistry;
@@ -20,23 +36,31 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.Is.is;
 
-
+/**
+ * Test for DetailsActivity
+ */
 public class DetailsActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
+    // Title of the ActionBar
     private static final String UNIPADERBORN = "Universität Paderborn";
-    private MainActivity mainactivity;
+    private static final String UNI_DESCRIPTION = "Die Universität Paderborn in Paderborn, " +
+            "Deutschland, ist eine 1972 gegründete Universität in Nordrhein-Westfalen.";
 
     public DetailsActivityTest() {
         super(MainActivity.class);
     }
 
-    /* help function for toolbar name matching */
+    /**
+     * Help function for toolbar name matching
+     *
+     * @param title the assertion to check.
+     * @return interaction for further perform/verification calls.
+     */
     private static ViewInteraction matchToolbarTitle(CharSequence title) {
         return onView(isAssignableFrom(Toolbar.class))
                 .check(matches(withToolbarTitle(is(title))));
     }
 
-    /* help function for toolbar name matching */
     private static Matcher<Object> withToolbarTitle(
             final Matcher<CharSequence> textMatcher) {
         return new BoundedMatcher<Object, Toolbar>(Toolbar.class) {
@@ -53,35 +77,51 @@ public class DetailsActivityTest extends ActivityInstrumentationTestCase2<MainAc
         };
     }
 
+    /**
+     * Sets up the fixture. This method is called before a test is executed.
+     *
+     * @throws Exception
+     */
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-        mainactivity = getActivity();
-        onView(withId(R.id.mainRecyclerView)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(UNIPADERBORN)), click()));
+        getActivity();
+        onView(withId(R.id.mainRecyclerView)).perform(RecyclerViewActions.
+                actionOnItem(hasDescendant(withText(UNIPADERBORN)), click()));
     }
 
-    /* test title name */
+    /**
+     * Test title name in ActionBar
+     */
     public void testActionBarTitle() {
         matchToolbarTitle(UNIPADERBORN);
     }
 
-    /* test name of the image */
+    /**
+     * Test name of the image
+     */
     public void testImageNameText() {
         onView(withId(R.id.detailsName)).check(matches(withText(UNIPADERBORN)));
     }
 
-    /* test the description */
+    /**
+     * Test the description
+     */
     public void testDescriptionText() {
-        onView(withId(R.id.detailsDescription)).check(matches(withText("Die Universität Paderborn in Paderborn, Deutschland, ist eine 1972 gegründete Universität in Nordrhein-Westfalen.")));
+        onView(withId(R.id.detailsDescription)).check(matches(withText(UNI_DESCRIPTION)));
     }
 
-    /* test if the image is disployed */
+    /**
+     * Test if the image is disployed
+     */
     public void testImage() {
         onView(withId(R.id.detailsImageView)).check(matches(isDisplayed()));
     }
 
-    /* test if the ImageViewDetail can be opened by clicking on the image */
+    /**
+     * Test if the ImageViewDetail can be opened by clicking on the image
+     */
     public void testOpenImageView() {
         onView(withId(R.id.detailsImageView)).perform(click());
         matchToolbarTitle(UNIPADERBORN);
