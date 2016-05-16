fe9bbef47743758738b87d84e1fd6937ad140457
jonaskir <jonaskir@mail.uni-paderborn.de>
2016-05-16 12:44:35 +0200

    Adds AppetizerExhibitPageFragment for testing purposes

---
+++ app/src/main/java/de/upb/hip/mobile/fragments/exhibitpagefragments/AppetizerExhibitPageFragment.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2016 History in Paderborn App - Universität Paderborn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.upb.hip.mobile.fragments.exhibitpagefragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.upb.hip.mobile.activities.R;
+import de.upb.hip.mobile.fragments.bottomsheetfragments.SimpleBottomSheetFragment;
+import de.upb.hip.mobile.helpers.BottomSheetConfig;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AppetizerExhibitPageFragment extends ExhibitPageFragment {
+
+    /** Title for the appetizer bottom sheet */
+    private String appetizerTitle = "default appetizer title";
+
+    /** Appetizer text displayed in the bottom sheet */
+    private String appetizerText = "default appetizer text";
+
+
+    public AppetizerExhibitPageFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exhibitpage_dummy, container, false);
+    }
+
+    @Override
+    public BottomSheetConfig getBottomSheetConfig() {
+        SimpleBottomSheetFragment bsFragment = new SimpleBottomSheetFragment();
+        bsFragment.setTitle(appetizerTitle);
+        bsFragment.setDescription(appetizerText);
+
+        return new BottomSheetConfig.Builder()
+                .maxHeight(210)
+                .peekHeight(210)
+                .fabAction(BottomSheetConfig.FabAction.NEXT)
+                .bottomSheetFragment(bsFragment)
+                .getBottomSheetConfig();
+    }
+
+    @Override
+    public Type getType() {
+        return Type.APPETIZER;
+    }
+}
