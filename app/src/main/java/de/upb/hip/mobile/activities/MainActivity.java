a5e22c133287085da1844867b7a853239eed2e38
Maximilian Kiehl <mkiehl@mail.uni-paderborn.de>
2016-06-07 09:37:38 +0200

    Make app compatible with permissions system of android 6

--- app/src/main/java/de/upb/hip/mobile/activities/MainActivity.java
+++ app/src/main/java/de/upb/hip/mobile/activities/MainActivity.java
@@ -16,9 +16,13 @@
 
 package de.upb.hip.mobile.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
@@ -92,6 +96,21 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check if we have the necessary permissions and request them if we don't
+        // Note that the app will still fail on first launch and needs to be restarted
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    0);
+        }
 
         mGpsTracker = new ExtendedLocationListener(MainActivity.this);
 
